5fd97d71db0ae91a7cc9cef797f5da4c7a83355a
robin2309 <robin.besson1@gmail.com>
2019-01-12 12:40:40 +0100

    remove eslint jest plugin

--- install-eslint.sh
+++ install-eslint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-npm install --save-dev babel-eslint eslint eslint-config-standard eslint-plugin-import eslint-plugin-jest eslint-plugin-node eslint-plugin-promise eslint-plugin-standard
+npm install --save-dev babel-eslint eslint eslint-config-standard eslint-plugin-import eslint-plugin-node eslint-plugin-promise eslint-plugin-standard
